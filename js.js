28600af14d34badcd51be127733bff9e946c6717
surya programmer <codewithsurya7@gmail.com>
2022-02-09 16:16:31 +0530

    f

---
+++ js.js
@@ -0,0 +1,54 @@
+// fetch("wp.json") .then(res=>res.json())
+//             .then(data =>{
+              
+
+
+// for (var i=5; i >= 0; i--) {
+    
+    
+
+// let a = document.createElement("div")
+// a.id="di"
+
+// a.innerHTML +=`
+// </div>
+// <div id="cardout">
+// <div id="card">
+//     <div id="left"><h4>${data[i].lec}</h4>
+//     <button class="button-71"  " id="watch">
+  
+    
+//    <a href="${data[i].watch}">watch</a>
+
+
+    
+//     </button>
+//     <button class="button-71"  id="download">Download</button>
+    
+//     </div>
+//     <div id="right">
+//         <div id="leftpart"><span>${data[i].day}</span></div>
+//         <div id="leftpart"><span>${data[i].date}</span></div>
+//         <div id="leftpart"><span>${data[i].type}</span></div>
+
+
+//     </div>
+// </div>
+// </div>
+
+
+// `;
+// let root = document.getElementById("root");
+// root.appendChild(a);
+
+
+
+// }
+
+// // console.log(data);
+
+// // console.log(data[0].id);
+
+
+//               })
+
